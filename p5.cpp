61b68b3eaaf1f2dc53dc0c0040a7f16d13bbe0c0
alainlou <az2lou@uwaterloo.ca>
2019-04-04 00:01:04 -0400

    added brute force solution for p5

---
+++ p5.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        bool value = true;
+        int len = s.length();
+        for(int i = 0; i < len; i++){
+            if(s[i] != s[len-1-i]){
+                value = false;
+                break;
+            }
+        }
+        return value;
+    }
+    
+    string longestPalindrome(string s) {
+        string toReturn = "";
+        int len = s.length();
+        for(int i = len; i > 0; i--){
+            int start = 0;
+            while(start + i <= len){
+                if(isPalindrome(s.substr(start, i))){
+                    return s.substr(start, i);
+                }
+                start++;
+            }
+        }
+        return toReturn;
+    }
+};
