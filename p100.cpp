e7128ee1be271e0321b3eaad75443b24a5080cd4
alainlou <az2lou@uwaterloo.ca>
2019-04-04 19:48:50 -0400

    added libraries and std namespace

--- p100.cpp
+++ p100.cpp
@@ -1,12 +1,14 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+#include <bits/stdc++.h>;
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
