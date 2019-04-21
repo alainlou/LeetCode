cf38c86bccb589d6424344377be2ec7ebe282ec5
alainlou <az2lou@uwaterloo.ca>
2019-04-21 09:18:31 -0400

    p14 is a badly composed problem just FYI

--- p14.cpp
+++ p14.cpp
@@ -4,29 +4,16 @@ using namespace std;
 
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        if(strs.size() == 0) 
-            return "";
-        size_t length = 0;
-        size_t min_length = SIZE_MAX;
-        for(string s : strs) {
-            if(s.size() < min_length) {
-                min_length = s.size();
-            }
+    int reverse(int x) {
+        int answer = 0;
+        while(x != 0){
+            int digit = x%10;
+            x /= 10;
+            if (answer > INT_MAX/10 || answer < INT_MIN/10)
+                return 0;
+            answer *= 10;
+            answer += digit;
         }
-        bool stop = false;
-        for(size_t i {0}; i < min_length; ++i){
-            char chr = strs[0][i];
-            for(size_t j {1}; j < strs.size(); ++j) {
-                if(strs[j][i] != chr) {
-                    stop = true;
-                }
-            }
-            if(stop) {
-                break;
-            }
-            ++length;
-        }
-        return strs[0].substr(0, length);
+        return answer;
     }
 };
