09e9e13a714cd63546c5ffdfbe48c72ee36995df
alainlou <az2lou@uwaterloo.ca>
2019-05-16 23:20:28 -0400

    2019/05/16

---
+++ p160.cpp
@@ -0,0 +1,21 @@
+#include <ListNode.hpp>
+
+using namespace std;
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        set<ListNode *> nodes;
+        while(headA) {
+            nodes.insert(headA);
+            headA = headA->next;
+        }
+        while(headB) {
+            if(nodes.count(headB) > 0) {
+                return headB;
+            }
+            headB = headB->next;
+        }
+        return NULL;
+    }
+};
