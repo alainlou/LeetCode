41cccdd2a004b9e7c49f60b4e469f5975e2551d8
alainlou <az2lou@uwaterloo.ca>
2019-05-23 23:03:30 -0400

    2019/05/23

---
+++ p287.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i < n; ++i) {
+            for(int j = i+1; j < n; ++j) {
+                if(nums[i] == nums[j]) {
+                    return nums[i];
+                }
+            }
+        }
+        return 0;
+    }
+};
