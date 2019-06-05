696ea194d9763f3519141e916f2efaba0793df85
alainlou <az2lou@uwaterloo.ca>
2019-06-04 23:02:04 -0400

    O(n) solution

--- p42.cpp
+++ p42.cpp
@@ -6,20 +6,24 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int counter = 0;
+        vector<int> left_max = {0};
+        vector<int> right_max = {0};
+        int left_carry = 0;
+        int right_carry = 0;
+        for(int l = 1; l < height.size(); ++l) {
+            if(height[l-1] > left_carry) {
+                left_carry = height[l-1];
+            }
+            left_max.push_back(left_carry);
+        }
+        for(int r = height.size() - 2; r >= 0; --r) {
+            if(height[r+1] > right_carry) {
+                right_carry = height[r+1];
+            }
+            right_max.insert(right_max.begin(), right_carry);
+        }
         for(int i = 0; i < height.size(); ++i) {
-            int left_max = 0;
-            int right_max = 0;
-            for(int l = i - 1; l >= 0; --l) {
-                if(height[l] > left_max) {
-                    left_max = height[l];
-                }
-            }
-            for(int r = i + 1; r < height.size(); ++r) {
-                if(height[r] > right_max) {
-                    right_max = height[r];
-                }
-            }
-            counter += max(0, min(left_max - height[i], right_max - height[i]));
+            counter += max(0, min(left_max[i] - height[i], right_max[i] - height[i]));
         }
         return counter;
     }
