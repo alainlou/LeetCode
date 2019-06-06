50cbbdce1e4233922a7fa1b1a61803740c5e70bd
alainlou <az2lou@uwaterloo.ca>
2019-06-06 00:33:12 -0400

    done the question

--- p114.cpp
+++ p114.cpp
@@ -1,4 +1,25 @@
 #include <bits/stdc++.h>
+#include <TreeNode.hpp>
 
 using namespace std;
 
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if(root == NULL) {
+            return;
+        }
+        flatten(root->left);
+        flatten(root->right);
+        if(!root->left) {
+            return;
+        }
+        TreeNode* curr = root->left;
+        while(curr->right) {
+            curr = curr->right;
+        }
+        curr->right = root->right;        
+        root->right = root->left;
+        root->left = NULL;
+    }
+};
