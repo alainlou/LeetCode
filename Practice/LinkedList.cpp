811aeecf33aff218bc47e9f985c99f4e963c0dd7
alainlou <az2lou@uwaterloo.ca>
2019-06-08 12:54:04 -0400

    LinkedList implementation

---
+++ Practice/LinkedList.cpp
@@ -0,0 +1,60 @@
+#include <cstddef>
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int i) : val(i), next(NULL) { }
+};
+
+struct LinkedList {
+    ListNode* head;
+    LinkedList(int i) : head(new ListNode(i)) { }
+    void insert(int i) {
+        ListNode* curr = head;
+        while(curr->next) {
+            curr = curr->next;
+        }
+        curr->next = new ListNode(i);
+    }
+    void reverse() {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* next;
+        while(curr) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+    void print() {
+        ListNode* curr = head;
+        while(curr) {
+            std::cout << curr->val << " -> ";
+            curr = curr->next;
+        }
+        std::cout << "END" << std::endl;
+    }
+};
+
+int main () {
+    // Instantiate a LinkedList
+    LinkedList* list = new LinkedList(1);
+
+    // Insert some nodes
+    list->insert(2);
+    list->insert(3);
+    list->insert(4);
+    
+    // Print the LinkedList
+    list->print();
+
+    // Reverse the LinkedList
+    list->reverse();
+
+    // Print the LinkedList to check the reverse worked
+    list->print();
+    return 0;
+}
