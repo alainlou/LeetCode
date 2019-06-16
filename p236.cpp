bc9b9596017e1076a0c4ef07328dbbb87f418980
alainlou <az2lou@uwaterloo.ca>
2019-06-16 13:17:11 -0400

    proper solution

--- p236.cpp
+++ p236.cpp
@@ -5,38 +5,21 @@ using namespace std;
 
 class Solution {
 public:
-    bool find(TreeNode* root, TreeNode* node, map<TreeNode*, bool>& vec) {
+    TreeNode* answer;
+    bool traverse(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root == NULL) {
-            vec[root] = false;
             return false;
-        } else if(root == node) {
-            vec[root] = true;
-            return true;
         }
-        bool result = find(root->left, node, vec) || find(root->right, node, vec);
-        vec[root] = result;
-        return result;
-    }
-    void find_lowest(TreeNode* root, TreeNode* p, TreeNode * q, map<TreeNode*, bool> p_included, map<TreeNode*, bool> q_included, TreeNode*& answer) {
-        if(root == NULL) {
-            return;
+        bool mid = root == p || root == q;
+        bool left = traverse(root->left, p, q);
+        bool right = traverse(root->right, p, q);
+        if(mid + left + right >= 2) {
+            answer = root;
         }
-        if(p_included[root] && q_included[root] && 
-           !(p_included[root->left] && q_included[root->left]) &&
-           !(p_included[root->right] && q_included[root->right])) {
-               answer = root;
-           }
-        find_lowest(root->left, p, q, p_included, q_included, answer);
-        find_lowest(root->right, p, q, p_included, q_included, answer);
+        return (mid + left + right > 0);
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        map<TreeNode*, bool> p_included;
-        map<TreeNode*, bool> q_included;
-        find(root, p, p_included);
-        find(root, q, q_included);
-        //traverse and find the lowest
-        TreeNode* answer;
-        find_lowest(root, p, q, p_included, q_included, answer);
+        traverse(root, p, q);
         return answer;
     }
 };
