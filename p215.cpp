c7242e801850d9aa99a201cfcd6429a8d53fb9b9
alainlou <az2lou@uwaterloo.ca>
2019-06-17 22:17:32 -0400

    prefer dynamic memory allocation

--- p215.cpp
+++ p215.cpp
@@ -5,7 +5,27 @@ using namespace std;
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        return nums[nums.size()-k];
+        int minimum = INT_MAX;
+        int maximum = INT_MIN;
+        // finding the minimum and maximum
+        for(int n : nums) {
+            minimum = min(minimum, n);
+            maximum = max(maximum, n);
+        }
+        // number of elements in the count array
+        int n = maximum - minimum + 1;
+        int * count = new int[n];
+        memset(count, 0, n*sizeof(int));
+        for(int num : nums) {
+            ++count[num - minimum];
+        }
+        int counter = 0;
+        for(int i = n - 1; i >= 0; --i) {
+            counter += count[i];
+            if(counter >= k) {
+                return i + minimum;
+            }
+        }
+        return -1;
     }
 };
