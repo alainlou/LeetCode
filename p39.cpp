037ce48c9fd70380b8cce003d0087f3546e4f074
alainlou <az2lou@uwaterloo.ca>
2019-06-20 00:59:07 -0400

    more nicely optimized p39

--- p39.cpp
+++ p39.cpp
@@ -5,32 +5,22 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> solution;
-    void generate(vector<int> curr, vector<int>& candidates, int target, int i, int& n) {
-        if(target < 0)
-            return;
-        if(curr.size() >= n) {
-            if(target == 0)
-                solution.push_back(curr);
-            return;
+    vector<int> candidates;
+    void generate(vector<int>& curr, int target, int start) {
+        if(target == 0) {
+            solution.push_back(curr);
         }
-        vector<int> alt1 = curr;
-        alt1.push_back(candidates[i]);
-        generate(alt1, candidates, target-candidates[i], i, n);
-        if(i < candidates.size() - 1) {
-            vector<int> alt2 = curr;
-            generate(alt2, candidates, target, i+1, n);
+        for(int i = start; i < candidates.size() && candidates[i] <= target; ++i) {
+            curr.push_back(candidates[i]);
+            generate(curr, target - candidates[i], i);
+            curr.pop_back();
         }
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
-        // this is the upper limit for size of one group
-        int n = target/candidates[0];
-        // generate all possible permutations and verify
-        for(int size = 1; size <= n; ++size) {
-            for(int start = 0; start < candidates.size(); ++start) {  
-                generate({candidates[start]}, candidates, target-candidates[start], start, size);
-            }
-        }
+        this->candidates = candidates;
+        vector<int> tmp;
+        generate(tmp, target, 0);
         return solution;
     }
 };
