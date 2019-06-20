b1854c25f45a25db36bd4c91b8697d9811b77873
alainlou <az2lou@uwaterloo.ca>
2019-06-19 23:20:33 -0400

    p22 is optimized a little more

--- p22.cpp
+++ p22.cpp
@@ -16,19 +16,21 @@ public:
         }
         return diff == 0;
     }
-    void generate(string s, vector<string>& result, int n) {
+    void generate(string s, int diff, vector<string>& result, int n) {
+        if(diff < 0)
+            return;
         if(s.size() == n) {
             if(valid(s)) {
                 result.push_back(s);
             }
         } else {
-            generate(s + "(", result, n);
-            generate(s + ")", result, n);
+            generate(s + "(", diff+1, result, n);
+            generate(s + ")", diff-1, result, n);
         }
     }
     vector<string> generateParenthesis(int n) {
         vector<string> answer;
-        generate("", answer, 2*n);
+        generate("", 0, answer, 2*n);
         return answer;
     }
 };
