3ca1afebc07087b3a5ef3974bc4b897994442721
alainlou <az2lou@uwaterloo.ca>
2019-06-28 06:28:40 -0400

    done p266

---
+++ p266.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == NULL)
+            return NULL;
+        TreeNode * left = root->left;
+        root->left = invertTree(root->right);
+        root->right = invertTree(left);
+        return root;
+    }
+};
