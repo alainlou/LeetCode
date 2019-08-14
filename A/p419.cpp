d7ce888f2169076852806367cf5f136912d024aa
alainlou <az2lou@uwaterloo.ca>
2019-08-13 23:52:27 -0400

    solve p419 with dp-ish

---
+++ A/p419.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        if(nums.size() == 0)
+            return false;
+        int total_sum = 0;
+        for(int n : nums) {
+            total_sum += n;
+        }
+        if(total_sum % 2 != 0)
+            return false;
+        int target = total_sum/2;
+        
+        bool possible[target+1];
+        memset(possible, false, sizeof(possible));
+        
+        // dp-ish
+        for(int n : nums) {
+            for(int i = target; i >= n; --i) {
+                if(possible[i-n]) {
+                    possible[i] = true;
+                }
+            }
+            if(n < target+1)
+                possible[n] = true;
+        }
+        
+        return possible[target];
+    }
+};
