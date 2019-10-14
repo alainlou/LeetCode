e7c40624aeab11cd8645e1b69a543520ce2dbe70
alainlou <az2lou@uwaterloo.ca>
2019-10-14 14:03:30 -0400

    solve p965

---
+++ p965.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isUnivalTree(TreeNode* root) {
+        if(root == NULL)
+            return true;
+        if(root->left != NULL && root->left->val != root->val || !isUnivalTree(root->left))
+             return false;
+        if(root->right != NULL && root->right->val != root->val || !isUnivalTree(root->right))
+            return false;
+        return true;
+    }
+};
