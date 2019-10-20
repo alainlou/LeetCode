24ad5aae35199cfe5b13e919d1fa9bf7a0e61780
alainlou <az2lou@uwaterloo.ca>
2019-10-20 10:17:46 -0400

    good taste

--- p1232.cpp
+++ p1232.cpp
@@ -5,14 +5,14 @@ using namespace std;
 class Solution {
 public:
     bool checkStraightLine(vector<vector<int>>& coordinates) {
-        double xd = coordinates[1][0] - coordinates[0][0];
-        double yd = coordinates[1][1] - coordinates[0][1];
-        double slope = yd/xd;
+        double xd0 = coordinates[1][0] - coordinates[0][0];
+        double yd0 = coordinates[1][1] - coordinates[0][1];
+        double slope0 = yd0/xd0;
         for(int i = 2; i < coordinates.size(); ++i) {
-            double xd1 = coordinates[i][0] - coordinates[i-1][0];
+            double xd = coordinates[i][0] - coordinates[i-1][0];
             double yd1 = coordinates[i][1] - coordinates[i-1][1];
-            double slope1 = yd1/xd1;
-            if(slope1 != slope)
+            double slope = yd1/xd;
+            if(slope != slope0)
                 return false;
         }
         return true;
