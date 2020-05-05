8b140a48ec27a26bc75975fda6fece42cdefd1a9
alainlou <az2lou@uwaterloo.ca>
2020-05-05 14:57:38 -0400

    may 5

---
+++ p387.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        count = defaultdict(int)
+
+        for c in s:
+            count[c] += 1
+
+        for i, c in enumerate(s):
+            if count[c] < 2:
+                return i
+
+        return -1
