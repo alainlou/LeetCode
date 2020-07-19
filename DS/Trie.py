d4f3a6566b6aff1b334c092b36f6dae333d99b3a
alainlou <az2lou@uwaterloo.ca>
2020-07-19 00:07:45 -0400

    make linter happy

--- DS/Trie.py
+++ DS/Trie.py
@@ -11,7 +11,7 @@ class Trie:
         node = self.root
         for c in s:
             index = ord(c)-ord('a')
-            if node.children[index] == None:
+            if node.children[index] is None:
                 node.children[index] = TrieNode()
             node = node.children[index]
         node.end = True
@@ -20,7 +20,7 @@ class Trie:
         node = self.root
         for c in s:
             index = ord(c)-ord('a')
-            if node.children[index] == None:
+            if node.children[index] is None:
                 return False
             node = node.children[index]
         return node.end
