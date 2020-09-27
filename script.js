366e990f0c23d83a2de24a23b3be65d0eca31915
alainlou <az2lou@uwaterloo.ca>
2020-09-26 23:31:17 -0400

    add scripts

---
+++ script.js
@@ -0,0 +1,6 @@
+let all = document.getElementsByTagName('td');
+arr = [];
+for (let e of all) {
+  if (e.innerText && !isNaN(e.innerText) && Number(e.innerText) < 1600) { arr.push(e.innerText); }
+}
+arr.toString();
