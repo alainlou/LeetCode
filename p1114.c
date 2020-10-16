988f6c471d7c0d4eb32bfb556cb99c161dfe9be5
alainlou <az2lou@uwaterloo.ca>
2020-10-16 12:28:15 -0400

    don't use busy waiting on p1114

--- p1114.c
+++ p1114.c
@@ -1,34 +1,46 @@
+#include <semaphore.h>
+
 typedef struct {
     // User defined data may be declared here.
-    int state;
+    sem_t * a;
+    sem_t * b;
 } Foo;
 
 Foo* fooCreate() {
     Foo* obj = (Foo*) malloc(sizeof(Foo));
+    obj->a = malloc(sizeof(sem_t));
+    obj->b = malloc(sizeof(sem_t));
+
     // Initialize user defined data here.
-    obj->state = 0;
+    sem_init(obj->a, 0, 0);
+    sem_init(obj->b, 0, 0);
     return obj;
 }
 
 void first(Foo* obj) {
     // printFirst() outputs "first". Do not change or remove this line.
     printFirst();
-    obj->state += 1;
+    sem_post(obj->a);
 }
 
 void second(Foo* obj) {
-    while (obj->state < 1) { }
     // printSecond() outputs "second". Do not change or remove this line.
+    sem_wait(obj->a);
     printSecond();
-    obj->state += 1;
+    sem_post(obj->b);
 }
 
 void third(Foo* obj) {
-    while (obj->state < 2) { }
     // printThird() outputs "third". Do not change or remove this line.
+    sem_wait(obj->b);
     printThird();
 }
 
 void fooFree(Foo* obj) {
     // User defined data may be cleaned up here.
+    sem_destroy(obj->a);
+    sem_destroy(obj->b);
+    free(obj->a);
+    free(obj->b);
+    free(obj);
 }
