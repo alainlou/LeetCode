d9ec918cc47b06b25106c08c253207c553d8372c
alainlou <az2lou@uwaterloo.ca>
2020-12-01 09:30:45 -0500

    dec 1

---
+++ p104.py
@@ -0,0 +1,7 @@
+from DS.TreeNode import TreeNode
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root == None:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
