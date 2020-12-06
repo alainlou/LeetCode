e680dd62c2d0b866e328317df3429575bfe8a413
alainlou <az2lou@uwaterloo.ca>
2020-12-06 10:06:50 -0500

    dec 6

---
+++ p117.py
@@ -0,0 +1,17 @@
+from DS.Node import Node
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if root == None:
+            return None
+        q = [(root, 0)]
+        while len(q) > 0:
+            curr = q.pop(0)
+            curr_node = curr[0]
+            curr_level = curr[1]
+            curr_node.next = None if len(q) == 0 or q[0][1] > curr_level else q[0][0]
+            if curr_node.left is not None:
+                q.append((curr_node.left, curr_level+1))
+            if curr_node.right is not None:
+                q.append((curr_node.right, curr_level+1))
+        return root
