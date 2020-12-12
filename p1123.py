177890e2e295d342306b546670bb8b431cb55793
alainlou <az2lou@uwaterloo.ca>
2020-12-12 18:35:18 -0500

    dec 12

---
+++ p1123.py
@@ -0,0 +1,23 @@
+from functools import lru_cache
+from DS.TreeNode import TreeNode
+
+class Solution:
+    def lcaDeepestLeaves(self, root: TreeNode) -> TreeNode:
+        @lru_cache(None)
+        def height(node):
+            if node is None:
+                return 0
+            return 1 + max(height(node.left), height(node.right))
+
+        def dfs(node):
+            if node is None:
+                return node
+            left, right = height(node.left), height(node.right)
+            if left == right:
+                return node
+            elif left > right:
+                return dfs(node.left)
+            else:
+                return dfs(node.right)
+
+        return dfs(root)
